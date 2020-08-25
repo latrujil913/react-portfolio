1718ac31b8fa8e69ca922cb53788cd9497ed80d8
Luis <ltluistrujillo25@gmail.com>
2020-08-25 12:10:31 -0700

    Added material icons

--- devport-app/src/components/Home.js
+++ devport-app/src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import FadeIn from "./animations/FadeIn";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import EmailIcon from "@material-ui/icons/Email";
 
 const HomeContainer = styled.div`
   min-height: 70vh;
@@ -13,6 +16,11 @@ const HomeContainer = styled.div`
   text-align: center;
 `;
 
+const IconBar = styled.div`
+  flex-direction: row;
+  text-align: center;
+`;
+
 class Home extends Component {
   render() {
     return (
@@ -21,6 +29,11 @@ class Home extends Component {
           <h1>Luis Trujillo</h1>
           <p>Software Engineer</p>
         </HomeContainer>
+        <IconBar>
+          <LinkedInIcon />
+          <GitHubIcon />
+          <EmailIcon />
+        </IconBar>
       </FadeIn>
     );
   }
